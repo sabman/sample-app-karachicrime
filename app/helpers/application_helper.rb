8bd98f3d0f75bb9b52a4541898fbc30e43d4239a
Shoaib Burq <saburq@gmail.com>
2012-01-29 19:01:42 +0100

    refactored the title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def title
     base_title = "KarachiCrime"
     variable_title = @title || action_name.capitalize
-    "#{base_title} | #{variable_title}"
+    [base_title, variable_title].join(' | ')
   end
 end
