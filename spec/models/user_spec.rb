ff5439d4be07c0d065641d67e44c84eda11f40b0
Shoaib Burq <saburq@gmail.com>
2012-01-30 23:01:34 +0100

    Added email validations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,4 +24,26 @@ describe User do
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
   end
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(email: address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(email: address))
+      valid_email_user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
 end
