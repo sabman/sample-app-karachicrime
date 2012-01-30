25a1dd1219ac2d93d4236d05348c43e70903b9c4
Shoaib Burq <saburq@gmail.com>
2012-01-30 09:32:20 +0100

    refactored logo and title helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
 module ApplicationHelper
+  def base_title
+    'KarachiCrime'
+  end
+
   def title
-    base_title = "KarachiCrime"
     variable_title = @title || action_name.capitalize
     [base_title, variable_title].join(' | ')
+  end
+
+  def logo
+    image_tag "logo.png", alt: base_title, class: "round"
   end
 end
