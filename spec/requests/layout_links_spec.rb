45bc666daea51e8a7efde3f306b065aeca79d7aa
Shoaib Burq <saburq@gmail.com>
2012-01-30 09:33:08 +0100

    Added further tests for testing correct pages are linked-to and capybara css selectors.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,27 +2,41 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   it "should have a Home at '/'" do
-    visit '/'
-    page.should have_content("Home")
+    visit root_path
+    page.should have_css('title', text: "Home")
   end
 
   it "should have a Contact page at '/contact'" do
-    visit '/contact'
-    page.should have_content("Contact")
+    visit contact_path
+    page.should have_css('title', text: "Contact")
   end
 
   it "should have a About page at '/about'" do
-    visit '/about'
-    page.should have_content("About")
+    visit about_path
+    page.should have_css('title', text: "About")
   end
 
   it "should have a Help page at '/help'" do
-    visit '/help'
-    page.should have_content("Help")
+    visit help_path
+    page.should have_css('title', text: "Help")
   end
 
   it "should have a signup page at '/signup" do
-    visit '/signup'
-    page.should have_content("Sign up")
+    visit signup_path
+    page.should have_css('title', text: "Sign up")
+  end
+
+  it "should have the right links" do
+    visit root_path
+    click_link "About"
+    page.should have_css('title', text: 'About')
+    click_link "Help"
+    page.should have_css('title', text: 'Help')
+    click_link "Contact"
+    page.should have_css('title', text: 'Contact')
+    click_link "Home"
+    page.should have_css('title', text: 'Home')
+    click_link "Sign up now!"
+    page.should have_css('title', text: 'Sign up')
   end
 end
