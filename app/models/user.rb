1357d8dcb0eb9bc2f572cf8bf3c601cb163d453a
Shoaib Burq <saburq@gmail.com>
2012-01-31 10:03:47 +0100

    added password as a virtual attribute and password_confirmation via confim validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -24,4 +25,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format: { with: email_regex },
                     uniqueness: {case_sensitive: false}
+  validates :password, confirmation: true,
+                       presence: true,
+                       length: { within: 6..40 }
 end
