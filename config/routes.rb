361959eeb2f5a5b59432833fcb83f6cca716cb91
Shoaib Burq <saburq@gmail.com>
2012-02-17 20:27:49 +0100

    Renamed pages_* to static_pages_*

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ SampleApp::Application.routes.draw do
   resources :users
 
   match '/signup', to: 'users#new'
-  match '/contact', to: 'pages#contact'
-  match '/about', to: 'pages#about'
-  match '/help', to: 'pages#help'
-  root to: 'pages#home'
+  match '/contact', to: 'static_pages#contact'
+  match '/about', to: 'static_pages#about'
+  match '/help', to: 'static_pages#help'
+  root to: 'static_pages#home'
 
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  get "static_pages/home"
+  get "static_pages/contact"
+  get "static_pages/about"
+  get "static_pages/help"
 end
