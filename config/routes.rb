0c7c055331bb7c192fd7bb2931a0a5c5a8a5706d
Shoaib Burq <saburq@gmail.com>
2012-02-19 21:36:22 +0100

    added specs for signin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy'
 
   match '/signup', to: 'users#new'
   match '/home', to: 'static_pages#home'
