e8e2b30ae8025c818634ac0b7cb387efb9ebe39f
Shoaib Burq <saburq@gmail.com>
2012-03-03 14:25:07 +0100

    added redirect for unauthorized users.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,6 +20,10 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   private
     def user_from_remember_token
       remember_token = cookies[:remember_token]
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -58,5 +58,21 @@ describe "Authentication" do
         end
       end
     end
+
+    describe "as a wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: 'wrong@example.com') }
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should have_selector('title', text: "Home") }
+      end
+
+      describe "submitting a PUT request to Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
   end
 end
